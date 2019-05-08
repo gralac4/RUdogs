bcab341b3fb80fd12905feeaafa83dfbfb5e017a
Alyssa Kutney <kutneya1@elvix2.rowan.edu>
2019-05-08 15:27:57 -0400

    Fixed rating so now it shows in the table

--- logADog.php
+++ logADog.php
@@ -27,21 +27,21 @@
 			<br>
 			<font size="2">**Please respect service dogs. Petting a service animal might be dangerous for the human they work with**</font>
 			<br>
-			<input type="radio" name="serviceDog" value=0>Yes
-			<input type="radio" name="serviceDog" value=1>No<br>
+			<input type="radio" name="serviceDog" value=1>Yes
+			<input type="radio" name="serviceDog" value=0>No<br>
 			<br>
 			<font size="5">Was the owner okay with pets?</font>
 			<br>
-			<input type="radio" name="permissionToPet" value=0>Yes
-			<input type="radio" name="permissionToPet" value=1>No
+			<input type="radio" name="permissionToPet" value=1>Yes
+			<input type="radio" name="permissionToPet" value=0>No
 			<input type="radio" name="permissionToPet" value=2>Unsure<br>
 			<br>
 			<font size="5">Was it a good dog?</font>
 			<br>
-			<input type="checkbox" name="rating" value ="rated" >10/10 would pet again
+			<input type="checkbox" name="rating" value =0 >10/10 an AMAZING DOGGO
 			<br>
 			<br>
-			<input type="submit" id="submitButton" value="Submit" onclick="pr()" disabled>
+			<input type="submit" id="submitButton" value="Submit" onclick="pr()" >
 		</form>	
 	</div>
 	<br><br><br><br><br><br><br><br>			
@@ -65,7 +65,8 @@
 			$permissionToPet = $_POST['permissionToPet'];
 			#$dateTime = date_create()->format('Y-m-d H:i:s');
 			$dateTime = date('Y-m-d H:i:s');
-			$insertSQL = "INSERT INTO campus_dogs (location, dateTime, dogOnDuty, permissionToPet) VALUES('$location','$dateTime','$dogOnDuty','$permissionToPet')";
+			$rating = $_POST['rating'];
+			$insertSQL = "INSERT INTO campus_dogs (location, dateTime, dogOnDuty, permissionToPet, rating) VALUES('$location','$dateTime','$dogOnDuty','$permissionToPet','$$rating')";
 
 		if($connection->query($insertSQL)===TRUE){
 				echo "Thank you for logging a doggo at " . $location ;
